a6c0783270406bff1e3c36f05701a70bfb021305
ential <xdnotavailable@gmail.com>
2025-10-20 14:51:54 +0530

    add hangman art

---
+++ main.py
@@ -0,0 +1,28 @@
+import random 
+words = ()
+hangman_art = {
+    0: ("   ",
+        "   ",
+        "   "),
+    1: (" o ",
+        "   ",
+        "   "),
+    2: (" o ",
+        " | ",
+        "   "),
+    3: (" o ",
+        "/| ",
+        "   "),
+    4: (" o ",
+        "/|\\",
+        "   "),
+    5: (" o ",
+        "/|\\",
+        "/  "),
+    6: (" o ",
+        "/|\\",
+        "/ \\")
+}
+for i in range(7):
+    for line in hangman_art[i]:
+        print (line)
